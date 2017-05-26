55a3a8b98fb3cce6a82fb150dbf8f552c8c9c0f1
Austin Quach <austinquachh@gmail.com>
2017-05-26 02:59:10 -0700

    Add nav bar

--- ui.R
+++ ui.R
@@ -6,20 +6,38 @@ shinyUI(fluidPage(
   # Add a descriptive application title
   titlePanel("Spotify Audio Features"),
   
-  sidebarLayout(
-    sidebarPanel(
-      
-      selectInput(inputId = "features",
-                  label = "Features:",
-                  choices = c("Danceability", "Energy", "Tempo"),
-                  selected = "Danceability")
-      
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      #give your plot a descriptive name
-      plotOutput("rolePlot")
-    )
+  navbarPage("Team inflatable Pizza",
+             tabPanel("Plot",
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                          selectInput(inputId = "features",
+                                      label = "Features:",
+                                      choices = c("Danceability", "Energy", "Tempo"),
+                                      selected = "Danceability")
+                          
+                        ),
+                        
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                          #give your plot a descriptive name
+                          plotOutput("rolePlot")
+                        )
+                      )
+                      
+                      ),
+             tabPanel("Summary"),
+             tabPanel("Table"),
+             tabPanel("Search", 
+                      # Text input box
+                      textInput("text", label = h3("Search"), value = "Enter text..."),
+                      
+                      # Action button
+                      actionButton("action", label = "Search"),
+                      
+                      hr(),
+                      fluidRow(column(2, verbatimTextOutput("value")))
+                      
+                      )
   )
 ))
