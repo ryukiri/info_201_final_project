ed2f4db02d7b7df56f69c802e2111916b9e7c5f4
vaneshsu <vaneshsu@gmail.com>
2017-05-29 10:48:59 -0700

    fixed get track id function

--- functions/GetTrackID.R
+++ functions/GetTrackID.R
@@ -1,9 +1,15 @@
+library(httr)
+library(jsonlite)
+
 GetTrackID <- function(my.song) {
+  keys <- spotifyOAuth("Info 201","ae706b417cc645f78c559186204dadd4","5f5769652ae24ceca43e05074b8b84eb")
+  
   track.params <- list(q = my.song, type = "track", limit = 1)
-  track.response <- GET("https://api.spotify.com/v1/search", query = track.params)
+  track.response <- httr::GET("https://api.spotify.com/v1/search", query = track.params, httr::config(token = keys))
   track.body <- content(track.response, "text")
   track.data <- fromJSON(track.body)
   track.df <- as.data.frame(track.data$tracks[2])
   track.id <- track.df$items.id
   return(track.id)
 }
+
