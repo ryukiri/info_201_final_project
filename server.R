b5b356c30d5423d0e72ba29b30b0ef22afb4c6e4
ZKeiLin <zlin2016@uw.edu>
2017-05-31 14:56:00 -0700

    organize code

--- server.R
+++ server.R
@@ -56,45 +56,7 @@ shinyServer(function(input, output) {
   songs.1980 <- read.csv("Songs/Songs - 1980.csv")
   
   ## Grabbing audio features for the years and putting them into respective data frames
-  # features.2016 <- read.csv("features/features.2016.csv")
-  # features.2015 <- read.csv("features/features.2015.csv")
-  # features.2014 <- read.csv("features/features.2014.csv")
-  # features.2013 <- read.csv("features/features.2013.csv")
-  # features.2012 <- read.csv("features/features.2012.csv")
-  # features.2011 <- read.csv("features/features.2011.csv")
-  # features.2010 <- read.csv("features/features.2010.csv")
-  # features.2009 <- read.csv("features/features.2009.csv")
-  # features.2008 <- read.csv("features/features.2008.csv")
-  # features.2007 <- read.csv("features/features.2007.csv")
-  # features.2006 <- read.csv("features/features.2006.csv")
-  # features.2005 <- read.csv("features/features.2005.csv")
-  # features.2004 <- read.csv("features/features.2004.csv")
-  # features.2003 <- read.csv("features/features.2003.csv")
-  # features.2002 <- read.csv("features/features.2002.csv")
-  # features.2001 <- read.csv("features/features.2001.csv")
-  # features.2000 <- read.csv("features/features.2000.csv")
-  # features.1999 <- read.csv("features/features.1999.csv")
-  # features.1998 <- read.csv("features/features.1998.csv")
-  # features.1997 <- read.csv("features/features.1997.csv")
-  # features.1996 <- read.csv("features/features.1996.csv")
-  # features.1995 <- read.csv("features/features.1995.csv")
-  # features.1994 <- read.csv("features/features.1994.csv")
-  # features.1993 <- read.csv("features/features.1993.csv")
-  # features.1992 <- read.csv("features/features.1992.csv")
-  # features.1991 <- read.csv("features/features.1991.csv")
-  # features.1990 <- read.csv("features/features.1990.csv")
-  # features.1989 <- read.csv("features/features.1989.csv")
-  # features.1988 <- read.csv("features/features.1988.csv")
-  # features.1987 <- read.csv("features/features.1987.csv")
-  # features.1986 <- read.csv("features/features.1986.csv")
-  # features.1985 <- read.csv("features/features.1985.csv")
-  # features.1984 <- read.csv("features/features.1984.csv")
-  # features.1983 <- read.csv("features/features.1983.csv")
-  # features.1982 <- read.csv("features/features.1982.csv")
-  # features.1981 <- read.csv("features/features.1981.csv")
-  # features.1980 <- read.csv("features/features.1980.csv")
-
-  ## Get all the features for hte years nad put them into data frame (import all the csv file in "features" folder)
+  ## (import all the csv file in "features" folder)
   temp = list.files(path = paste0(getwd(), '/features'),pattern="*.csv", full.names = TRUE)
   list2env(
     lapply(setNames(temp, make.names(gsub("*.csv", "", gsub(paste0(getwd(), '/features'), "", temp) ))), 
@@ -145,7 +107,6 @@ shinyServer(function(input, output) {
   ## Merge them into one for use in some graphs
   features.all.songs <- read.csv("songsMerged/songs.merged.all.csv")
   
-
   ## Calculate danceability averages
   features.2016.danceability <- mean(X.features.2016$danceability)
   features.2015.danceability <- mean(X.features.2015$danceability)
@@ -498,8 +459,6 @@ shinyServer(function(input, output) {
   features.1981.duration_ms <- mean(X.features.1981$duration_ms)
   features.1980.duration_ms <- mean(X.features.1980$duration_ms)
 
-  
-  songs.merged.all <- read.csv("songsMerged/songs.merged.all.csv")
     # Select audio features to show
     if(input$features == "Danceability") {
         dat <- data.frame(
@@ -543,7 +502,7 @@ shinyServer(function(input, output) {
                          features.2015.danceability,
                          features.2016.danceability))
         all.years.feature <-Reduce(function(...) merge(..., by='X', all=T), lapply(names(all.features), Merge.feature.year, 'danceability'))
-        feature.years <- songs.merged.all%>%select(feature = danceability, Year, Song)
+        feature.years <- features.all.songs%>%select(feature = danceability, Year)
     } else if(input$features == "Energy"){
         dat <- data.frame(
         year = factor(c(1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016), 
@@ -586,7 +545,7 @@ shinyServer(function(input, output) {
                          features.2015.energy,
                          features.2016.energy))
         all.years.feature <-Reduce(function(...) merge(..., by='X', all=T), lapply(names(all.features), Merge.feature.year, 'energy'))
-        feature.years <- songs.merged.all%>%select(feature = energy, Year, Song)
+        feature.years <- features.all.songs%>%select(feature = energy, Year)
         
     } else if(input$features == "Tempo"){
         dat <- data.frame(
@@ -630,7 +589,7 @@ shinyServer(function(input, output) {
                          features.2015.tempo,
                          features.2016.tempo))
         all.years.feature <-Reduce(function(...) merge(..., by='X', all=T), lapply(names(all.features), Merge.feature.year, 'tempo'))
-        feature.years <- songs.merged.all%>%select(feature = tempo, Year, Song)
+        feature.years <- features.all.songs%>%select(feature = tempo, Year)
 
     } else if(input$features == "Loudness"){
       dat <- data.frame(
@@ -674,7 +633,7 @@ shinyServer(function(input, output) {
                           features.2015.loudness,
                           features.2016.loudness))
         all.years.feature <-Reduce(function(...) merge(..., by='X', all=T), lapply(names(all.features), Merge.feature.year, 'loudness'))
-        feature.years <- songs.merged.all%>%select(feature = loudness, Year, Song)
+        feature.years <- features.all.songs%>%select(feature = loudness, Year)
 
     } else if(input$features == "Speechiness"){
       dat <- data.frame(
@@ -718,7 +677,7 @@ shinyServer(function(input, output) {
                          features.2015.speechiness,
                          features.2016.speechiness))
         all.years.feature <-Reduce(function(...) merge(..., by='X', all=T), lapply(names(all.features), Merge.feature.year, 'speechiness'))
-        feature.years <- songs.merged.all%>%select(feature = speechiness, Year, Song)
+        feature.years <- features.all.songs%>%select(feature = speechiness, Year)
 
     } else if(input$features == "Acousticness"){
       dat <- data.frame(
@@ -762,7 +721,7 @@ shinyServer(function(input, output) {
                           features.2015.acousticness,
                           features.2016.acousticness))
         all.years.feature <-Reduce(function(...) merge(..., by='X', all=T), lapply(names(all.features), Merge.feature.year, 'acousticness'))
-        feature.years <- songs.merged.all%>%select(feature = acousticness, Year, Song)
+        feature.years <- features.all.songs%>%select(feature = acousticness, Year)
 
     } else if(input$features == "Liveness"){
       dat <- data.frame(
@@ -806,7 +765,7 @@ shinyServer(function(input, output) {
                          features.2015.liveness,
                          features.2016.liveness))
         all.years.feature <-Reduce(function(...) merge(..., by='X', all=T), lapply(names(all.features), Merge.feature.year, 'liveness'))
-        feature.years <- songs.merged.all%>%select(feature = liveness, Year, Song)
+        feature.years <- features.all.songs%>%select(feature = liveness, Year)
 
     } else if(input$features == "Instrumentalness"){
       dat <- data.frame(
@@ -850,7 +809,7 @@ shinyServer(function(input, output) {
                          features.2015.instrumentalness,
                          features.2016.instrumentalness))
         all.years.feature <-Reduce(function(...) merge(..., by='X', all=T), lapply(names(all.features), Merge.feature.year, 'instrumentalness'))
-        feature.years <- songs.merged.all%>%select(feature =instrumentalness, Year, Song)
+        feature.years <- features.all.songs%>%select(feature = instrumentalness, Year)
     } else if(input$features == "Song Length"){
       dat <- data.frame(
         year = factor(c(1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016), 
@@ -893,27 +852,24 @@ shinyServer(function(input, output) {
                          features.2015.duration_ms,
                          features.2016.duration_ms))
       all.years.feature <-Reduce(function(...) merge(..., by='X', all=T), lapply(names(all.features), Merge.feature.year, 'duration_ms'))
-      feature.years <- songs.merged.all%>%select(feature = duration_ms, Year, Song)
+      feature.years <- features.all.songs%>%select(feature = duration_ms, Year)
     }
   
     if(input$plot_types == "Barplot") {
       ggplot(data=dat, aes(x=year, y=stat_average, fill=year)) +
         geom_bar(stat="identity") +
-        labs(y = input$features) +
-        theme(axis.text.x = element_text(angle = 90, vjust=0.5, hjust=0))
+        labs(y = input$features)
     } else if(input$plot_types == "Boxplot") {
       ggplot(stack(all.years.feature[,-1]), aes(x = ind, y = values, color = ind)) +
         geom_boxplot() +
-        labs(x = "Years", y = input$features, color = "Year") +
-        theme(axis.text.x = element_text(angle = 90, vjust=0.5, hjust=0))
+        labs(x = "Years", y = input$features, color = "Year")
     } else if(input$plot_types == "Quantile") {
       qplot(Year, feature, data = feature.years,
            xlab = 'Year', ylab = input$features,geom = c("point", "smooth"),span = 0.2,  col = Year)
     } else if(input$plot_types == "Violin") {
       ggplot(stack(all.years.feature[,-1]), aes(x = ind, y = values, color = ind)) +
         geom_violin() +
-        labs(x = "Years", y = input$features, color = 'Year') +
-        theme(axis.text.x = element_text(angle = 90, vjust=0.5, hjust=0))
+        labs(x = "Years", y = input$features, color = 'Year')
     }
   })
   
